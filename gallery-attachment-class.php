3ab21aee4c7fc64341455ceb939de358418b548e
Laura Camellini <jilt@jeeltcraft.com>
2017-04-05 19:29:17 +0200

    gallery class for amp-carousel add

---
+++ gallery-attachment-class.php
@@ -0,0 +1,57 @@
+add_filter('post_gallery', 'amp_carousel_gallery', 10, 2);
+function amp_carousel_gallery($output, $attr) {
+    global $post;
+
+    if (isset($attr['orderby'])) {
+        $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
+        if (!$attr['orderby'])
+            unset($attr['orderby']);
+    }
+
+    extract(shortcode_atts(array(
+        'order' => 'ASC',
+        'orderby' => 'menu_order ID',
+        'id' => $post->ID,
+        'itemtag' => 'dl',
+        'icontag' => 'dt',
+        'captiontag' => 'dd',
+        'columns' => 3,
+        'size' => 'medium-large',
+        'include' => '',
+        'exclude' => ''
+    ), $attr));
+
+    $id = intval($id);
+    if ('RAND' == $order) $orderby = 'none';
+
+    if (!empty($include)) {
+        $include = preg_replace('/[^0-9,]+/', '', $include);
+        $_attachments = get_posts(array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+
+        $attachments = array();
+        foreach ($_attachments as $key => $val) {
+            $attachments[$val->ID] = $_attachments[$key];
+        }
+    }
+
+    if (empty($attachments)) return '';
+
+    // Here's your actual output, you may customize it to your need
+    $output = "<amp-carousel height=\"500\"\n";
+    $output .= "layout=\"fixed-height\"\n";
+    $output .= "type=\"slides\">\n";
+
+    // Now you loop through each attachment
+    foreach ($attachments as $id => $attachment) {
+        // Fetch the thumbnail (or full image, it's up to you)
+//      $img = wp_get_attachment_image_src($id, 'medium');
+//      $img = wp_get_attachment_image_src($id, 'my-custom-image-size');
+        $img = wp_get_attachment_image_src($id, 'medium-large');
+
+        $output .= "<amp-img src=\"{$img[0]}\" height=\"{$img[2]}\" layout=\"fixed-height\" alt=\"\" ></amp-img>\n";
+    }
+
+    $output .= "</amp-carousel>\n";
+
+    return $output;
+}
