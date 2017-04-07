5bfb4819cdfcc8cc6418fbedde01119e236ea5e1
Laura Camellini <jilt@jeeltcraft.com>
2017-04-07 15:36:28 +0200

    regexp added

--- amp-youtube-embed.php
+++ amp-youtube-embed.php
@@ -1,18 +1,10 @@
-<amp-youtube width="480"
-  height="270"
-  layout="responsive"
-  data-videoid="lBTCB7yLs8Y">
-</amp-youtube>
-
-<iframe width="960" height="540" src="https://www.youtube.com/embed/RTJvgZ5bgpM?feature=oembed" frameborder="0" allowfullscreen>
-</iframe>
-
 add_filter( 'embed_oembed_html', 'amp_youtube_oembed' );
 function amp_youtube_oembed( $code ){
     if( stripos( $code, 'youtube.com' ) !== FALSE && stripos( $code, 'iframe' ) !== FALSE )
-        $code = str_replace( '<iframe', '<amp-youtube layout="fixed height" ', $code );
-        // reperire id video - reach video id
-        $code .="</amp-youtube>\n";
-
+        $url = urldecode(rawurldecode( $code ));
+            preg_match("/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/", $url, $matches);
+            $amp_y_id = echo $matches[1];
+        $code = str_replace( '<iframe', '<amp-youtube layout="fixed height" data-videoid="'. $amp_y_id .'"', $code );
+        $code = str_replace( '</iframe>', '</amp-youtube>', $code);
     return $code;
 }
