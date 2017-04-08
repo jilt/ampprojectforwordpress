f9dfa1c9d1da1f8f130cffb0d9ff8b4aa5e3fa3c
Laura Camellini <jilt@jeeltcraft.com>
2017-04-08 16:46:05 +0200

    fixes

--- amp-youtube-embed.php
+++ amp-youtube-embed.php
@@ -1,6 +1,6 @@
 add_filter( 'embed_oembed_html', 'amp_youtube_oembed' );
 function amp_youtube_oembed( $code ){
-    if( stripos( $code, 'youtube.com' ) !== FALSE && stripos( $code, 'iframe' ) !== FALSE )
+    if( stripos( $code, 'youtube.com' ) !== FALSE ) // stripos( $code, 'iframe' ) !== FALSE
         $url = urldecode(rawurldecode( $code ));
             preg_match("/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/", $url, $matches);
             $amp_y_id = echo $matches[1];
