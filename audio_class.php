d96054dab31d2d712a0f3b8aee3c593848fe0519
Laura Camellini <jilt@jeeltcraft.com>
2017-04-08 14:22:25 +0200

    added amp-audio syntax

--- audio_class.php
+++ audio_class.php
@@ -1,11 +1,15 @@
+//remember to add <script async custom-element="amp-audio" src="https://cdn.ampproject.org/v0/amp-audio-0.1.js"></script>
 
-function amp-audio-player( $html, $attr ) {
-    if ( ! empty( $attr['player'] ) && $attr['player'] === 'default' )
-        $html = '{audio player:default}';
-    else
-        $html = '{audio player:other}';
+function wp_amp_audio( $html, $atts, $audio, $post_id, $library ) {
+    $html .='<amp-audio width="auto"\n';
+    $html .='height="50"\n';
+    $html .='src="'.$atts['src'].'">\n';
+    $html .='<div fallback>\n';
+    $html .='<p>Your browser doesn’t support HTML5 audio</p>\n';
+    $html .='</div>\n';
+    $html .='</amp-audio>';
 
-    return "<p>$html</p>";
+    return $html;
+
 }
-
-add_filter( 'wp_audio_shortcode_override', 'amp-audio-player', 10, 2 );
+add_filter( 'wp_audio_shortcode','wp_amp_audio',5,10);
