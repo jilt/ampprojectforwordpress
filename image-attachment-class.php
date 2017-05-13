71090afd99a53da20e489610bf6bc873374ddd48
Laura Camellini <jilt@jeeltcraft.com>
2017-05-14 00:32:15 +0200

    recently tested method
    Need to add srcset and size vars

--- image-attachment-class.php
+++ image-attachment-class.php
@@ -1,46 +1,7 @@
 
-function amp_img_html_template($html, $id, $caption, $title, $align, $url, $size, $alt) {
-	/*
-	$html - default HTML, you can use regular expressions to operate with it
-	$id - attachment ID
-	$caption - image Caption
-	$title - image Title
-	$align - image Alignment
-	$url - link to media file or to the attachment page (depends on what you selected in media uploader)
-	$size - image size (Thumbnail, Medium, Large etc)
-	$alt - image Alt Text
-	*/
- 
-	/*
-	 * First of all lets operate with image sizes
-	 */
-	list( $img_src, $width, $height ) = image_downsize($id, $size);
-	$hwstring = image_hwstring($width, $height);
- 
-	/*
- 	 * Second thing - get the image URL $image_thumb[0]
-	 */
-	$image_thumb = wp_get_attachment_image_src( $id, $size );
-  $img_srcset = wp_get_attachment_image_srcset( $id, $size );
- 
-	$out = '<div class="img-wrapper">'; // I want to wrap image into this div element
-	if($url){ // if user wants to print the link with image
-		$out .= '<a href="' . $url . '" class="ampstart-btn" on="tap:img-lightbox" role="button" tabindex="0"';
-	}
-	$out .= '<amp-img src="'. $image_thumb[0] .'" alt="'.$alt.'" '.$hwstring.' '.$img_srcset[0].' layout="responsive"></amp-img>';
-	if($url){
-		$out .= '</a>\n';
-	}
-	$out .= '</div><amp-lightbox id="img-lightbox"\n';
-	$out .= 'layout="nodisplay">\n';
-  	$out .='<div class="lightbox"\n';
-	$out .='on="tap:img-lightbox.close"\n';
-	$out .='role="button"\n';
-	$out .='tabindex="0">\n';
-	$out .='<amp-img src="'. $image_thumb[0] .'" alt="'.$alt.'" '.$hwstring.' '.$img_srcset[0].' layout="responsive"></amp-img>\n';
-	$out .='</div>\n';
-	$out .='</amp-lightbox>\n';
-	return $out; // the result HTML
-}
- 
-add_filter('image_send_to_editor', 'amp_img_html_template', 1, 8);
+add_filter( 'image_send_to_editor', 'amp_img_editor', 10, 7);
+       function amp_img_editor($html, $id, $alt, $title, $align, $url, $size ) {
+           $url = wp_get_attachment_url($id); // Grab the current image URL
+           $html = '<amp-img src="' . $url .  '" layout="responsive" width="620" height="280"></amp-img>';
+           return $html;
+       }
