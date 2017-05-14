e3f8eaa5104a90685510ee9fcc0b82476c4eb490
Laura Camellini <jilt@jeeltcraft.com>
2017-05-14 14:14:15 +0200

    added PREG_OFFSET_CAPTURE, still broken

--- amp-youtube-embed.php
+++ amp-youtube-embed.php
@@ -2,7 +2,7 @@ add_filter( 'embed_oembed_html', 'amp_youtube_oembed' );
 function amp_youtube_oembed( $code ){
     if(strpos($code, 'youtu.be') !== false || strpos($code, 'youtube.com') !== false):
         $url = urldecode(rawurldecode( $code ));
-            preg_match("/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/", $url, $matches);
+            preg_match("/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/", $url, $matches, PREG_OFFSET_CAPTURE);
             $amp_y_id = echo $matches[1];
         $code = str_replace( '<iframe', '<amp-youtube layout="fixed height" data-videoid="'. $amp_y_id .'"', $code );
         $code = str_replace( '</iframe>', '</amp-youtube>', $code);
