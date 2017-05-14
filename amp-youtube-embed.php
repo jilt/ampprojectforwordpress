6625726667d3064eeec0ef1be96f4d7af6c70f87
Laura Camellini <jilt@jeeltcraft.com>
2017-05-14 12:10:38 +0200

    strpos fixes, still can't resolve $matches[1]

--- amp-youtube-embed.php
+++ amp-youtube-embed.php
@@ -1,21 +1,11 @@
 add_filter( 'embed_oembed_html', 'amp_youtube_oembed' );
 function amp_youtube_oembed( $code ){
-    if( stripos( $code, 'youtube.com' ) !== FALSE ) // stripos( $code, 'iframe' ) !== FALSE
+    if(strpos($code, 'youtu.be') !== false || strpos($code, 'youtube.com') !== false):
         $url = urldecode(rawurldecode( $code ));
             preg_match("/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/", $url, $matches);
             $amp_y_id = echo $matches[1];
         $code = str_replace( '<iframe', '<amp-youtube layout="fixed height" data-videoid="'. $amp_y_id .'"', $code );
         $code = str_replace( '</iframe>', '</amp-youtube>', $code);
     return $code;
+    endif;
 }
-//oembed_result alternative
-//function amp_youtube_oembed( $html, $url, $args ) {
- 
-	/* Modify video parameters. */
-//	if ( strstr( $html,'youtube.com/embed/' ) ) {
-//		$html = str_replace( 'xxx', 'yyy', $html );
-//	}
-	
-//    return $html;
-//}
-//add_filter( 'oembed_result', 'amp_youtube_oembed', 10, 3 );
